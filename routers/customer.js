70a0c6253030f496e69e56814444639b70500aa4
Isaac Obuya <Szack4477@gmail.com>
2020-09-18 09:26:59 +0300

    add a post request

--- routers/customer.js
+++ routers/customer.js
@@ -6,26 +6,26 @@ const Customer = require("../model/customer");
 const router = express.Router();
 
 // create a new customer
-// router.post('/customer', async (req, res) => {
-//     console.log(req.body);
-//     const customer = new Customer(req.body);
-//     try {
-//         const newCustomer = await customer.save();
-//         res.status(201).send(newCustomer);
-//     } catch(error) {
-//         res.status(400).send(error);
-//         console.log(error);
-//     }
-// });
+router.post("/customer", async (req, res) => {
+  console.log(req.body);
+  const customer = new Customer(req.body);
+  try {
+    const newCustomer = await customer.save();
+    res.status(201).send(newCustomer);
+  } catch (error) {
+    res.status(400).send(error);
+    console.log(error);
+  }
+});
 
 // get all customers
-router.get('/customers', async (req, res) => {
-    try {
-        const customers = await Customer.find({});
-        res.status(200).send({customers});
-    } catch (error) {
-        res.status(404).send(error);
-    }
+router.get("/customers", async (req, res) => {
+  try {
+    const customers = await Customer.find({});
+    res.status(200).send({ customers });
+  } catch (error) {
+    res.status(404).send(error);
+  }
 });
 
 module.exports = router;
